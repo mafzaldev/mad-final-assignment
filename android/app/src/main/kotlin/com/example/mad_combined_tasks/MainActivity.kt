def6219a67fb83b8260ac554365cc9ab8f1e1715
Afzal <afzalashraf814@gmail.com>
2023-04-24 00:17:21 +0500

    :sparkles:initial commit with firebase integration

---
+++ android/app/src/main/kotlin/com/example/mad_combined_tasks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mad_combined_tasks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:mad_combined_tasks/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MAD Combined Tasks',
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: const HomePage(),
+    );
+  }
+}
